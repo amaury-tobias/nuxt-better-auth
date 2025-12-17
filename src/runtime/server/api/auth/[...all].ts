cbe6f7832e4a8a6c80abf6d8c32701842bf3c65f
onmax <maximogarciamtnez@gmail.com>
2025-12-17 15:58:25 +0100

    fix: add explicit import for serverAuth in handler

--- src/runtime/server/api/auth/[...all].ts
+++ src/runtime/server/api/auth/[...all].ts
@@ -1,5 +1,6 @@
 import type { H3Event } from 'h3'
 import { defineEventHandler, toWebRequest } from 'h3'
+import { serverAuth } from '../../utils/auth'
 
 export default defineEventHandler(async (event: H3Event) => {
   const auth = await serverAuth()
